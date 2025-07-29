4fa2cc6c83554d5e6ed6c27ac5353b70e2a9db50
adarsh <adarshadarsh92085@gmail.com>
2025-07-29 11:32:15 +0530

    Add adarsh.py source file

---
+++ adarsh.py
@@ -0,0 +1,11 @@
+"""This module checks if a user is allowed to vote based on age."""
+
+print("hello")
+name = input("Enter the name: ")
+age = int(input("Enter the age: "))
+
+if age > 18:
+    print("Allowed to vote")
+    print("Allowed to vote")
+else:
+    print("Not allowed to vote")
